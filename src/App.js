b27ca6be4883aca8486bdc6d8ad79f18d616b183
Ali Rıza Yüksel <114735512+alirizayuksel66@users.noreply.github.com>
2022-11-30 12:29:53 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logom from "./logo.svg"
 function App() {
   return (
     <div className="App">
+      <h3>{process.env.NODE_ENV}</h3>
       Ali Rıza
       <img src={logom}></img>
     </div>
